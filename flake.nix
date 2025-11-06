5bc15e4f0398ceb93200f6f0d515f446996b0526
Thunder <thunder@disroot.org>
2025-11-06 19:32:06 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    crane.url = "github:ipetkov/crane";
+  };
+
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    crane,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
+
+    forAllSystems = fn:
+      lib.genAttrs lib.systems.flakeExposed
+      (system:
+        fn
+        (import nixpkgs {
+          inherit system;
+          overlays = [
+            (import rust-overlay)
+          ];
+        }));
+  in {
+    devShells = forAllSystems (pkgs: let
+      toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+    in {
+      default = pkgs.mkShell {
+        buildInputs = [toolchain];
+      };
+    });
+
+    packages = forAllSystems (pkgs: let
+      toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+
+      cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+    in {
+      default = craneLib.buildPackage {
+        inherit (cargoToml.package) version;
+        pname = cargoToml.package.name;
+        src = ./.;
+      };
+    });
+  };
+}
